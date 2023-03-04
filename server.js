d5d6de7313df2dabcf9f83507334461da6e0c983
ch15 <58167402+chsolito15@users.noreply.github.com>
2023-03-04 19:28:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) =>{
             console.log('Unable to append to server.log.');
         }
     })
-    next(); // when refresh page render quickly
+    next(); 
 });
 
 /* app.use((req, res, next) =>{
